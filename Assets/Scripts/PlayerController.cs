59381bb89a0568b6538a406f43d2d5406cfecdb7
josefhruska <pepa.hruska@gmail.com>
2016-12-30 22:13:15 +0100

    pff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,11 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
-	private RigidBody rb
-
-	void Start () {
-		rb = GetComponent<RigidBody>()
-	}
+	
 	// Update is called once per frame
 	void FixedUpdate () {
-		float moveHorizontal = Input.GetAxis ("Horizontal")
-		float moveVertical = Input.GetAxis ("Vertical")
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
 
 	}
 
