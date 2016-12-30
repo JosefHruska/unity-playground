bccc8de0eb9bd80f382dc5aed5401c4cdbb60ca2
josefhruska <pepa.hruska@gmail.com>
2016-12-30 21:56:16 +0100

    unity-test

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private RigidBody rb
+
+	void Start () {
+		rb = GetComponent<RigidBody>()
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal")
+		float moveVertical = Input.GetAxis ("Vertical")
+
+	}
+
+}
